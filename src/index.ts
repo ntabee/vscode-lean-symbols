5b8bd4143c2f1318fcb52585baf3bbcd77c6ef36
ntabee <ntabee@gmail.com>
2020-03-01 15:39:13 +0900

    First skelton

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-function hello(name: string): string {
-    return `Hello, ${name}!`;
+import fetch from 'node-fetch'
+const blob = 'https://raw.githubusercontent.com/leanprover/vscode-lean/master/translations.json'
+
+const main = async() => {
+  const mapping = await fetch(blob).then((r) => r.json())
+  console.log(mapping)
 }
 
-const name = 'Taro';
-console.log(hello(name));
+main()
