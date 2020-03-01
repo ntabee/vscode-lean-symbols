b80deb47e46bf0018f5f7088621b4744d21a5aab
ntabee <ntabee@gmail.com>
2020-03-01 18:39:36 +0900

    Build: an inverse mapping

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@ const blob = 'https://raw.githubusercontent.com/leanprover/vscode-lean/master/tr
 
 const main = async() => {
   const mapping = await fetch(blob).then((r) => r.json())
-  console.log(mapping)
+  let inverse: {[key:string]: string[]} = {}
+  for (let [k, v] of Object.entries(mapping)) {
+    if (typeof v == 'string')
+      inverse[v] = [...(inverse[v] || []), k]
+  }
+  console.log(mapping, inverse)
 }
 
 main()
